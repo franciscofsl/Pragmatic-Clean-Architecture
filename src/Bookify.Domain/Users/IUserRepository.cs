8f0452174dd3e3c7b574d47122d535922190e0d4
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-04-07 18:15:15 +0200

    2: Domain Layer - Adding Repositories and the Unit of Work to the domain

---
+++ src/Bookify.Domain/Users/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookify.Domain.Users;
+
+public interface IUserRepository
+{
+    Task<User?> GetUserByIdAsync(Guid userId, CancellationToken cancellationToken = default);
+
+    void Add(User user);
+}
