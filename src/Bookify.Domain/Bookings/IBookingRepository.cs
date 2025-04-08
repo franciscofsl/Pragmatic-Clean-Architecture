9d9259948de0c51a28bc9c95fd58d5aa289ded0f
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-04-08 17:50:02 +0200

    3: Application Layer - Defining a domain event handler

--- src/Bookify.Domain/Bookings/IBookingRepository.cs
+++ src/Bookify.Domain/Bookings/IBookingRepository.cs
@@ -6,4 +6,5 @@ public interface IBookingRepository
 {
     Task<bool> IsOverlappingAsync(Apartment apartment, DateRange duration, CancellationToken cancellationToken);
     void Add(Booking booking);
+    Task<Booking?> GetByIdAsync(Guid notificationBookingId, CancellationToken cancellationToken);
 }
