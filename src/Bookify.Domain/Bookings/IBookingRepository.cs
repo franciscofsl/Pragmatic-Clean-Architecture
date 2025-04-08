03f897efe0601a0d302f5c5ae0290827aef45be9
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-04-08 17:44:16 +0200

    3: Application Layer - Implementing Commands with the rich domain model & repositories

---
+++ src/Bookify.Domain/Bookings/IBookingRepository.cs
@@ -0,0 +1,9 @@
+﻿using Bookify.Domain.Apartments;
+
+namespace Bookify.Domain.Bookings;
+
+public interface IBookingRepository
+{
+    Task<bool> IsOverlappingAsync(Apartment apartment, DateRange duration, CancellationToken cancellationToken);
+    void Add(Booking booking);
+}
