41de5e5436915199d670751355e63c5382840619
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-04-09 18:13:20 +0200

    5: Presentation Layer - Setting up Docker Compose and running the API

--- src/Bookify.Api/Program.cs
+++ src/Bookify.Api/Program.cs
@@ -7,8 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
@@ -17,7 +18,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    
+    // app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
