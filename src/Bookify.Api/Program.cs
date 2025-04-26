f28926e47596abf8bf52ee62d8459e8c46308ff3
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-04-26 13:39:51 +0200

    8: Advanced Topics - Health Checks in Clean Architecture (custom)

--- src/Bookify.Api/Program.cs
+++ src/Bookify.Api/Program.cs
@@ -1,15 +1,17 @@
 using Bookify.Api.Extensions;
 using Bookify.Application;
+using Bookify.Application.Abstractions.Data;
 using Bookify.Infrastructure;
+using Dapper;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Host.UseSerilog((context, configuration) =>
-{
-    configuration.ReadFrom.Configuration(context.Configuration);
-});
+builder.Host.UseSerilog((context, configuration) => { configuration.ReadFrom.Configuration(context.Configuration); });
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -19,6 +21,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
+builder.Services.AddHealthChecks().AddCheck<CustomSqlHealthCheck>("custom-sql");
+;
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,7 +31,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    
+
     app.ApplyMigrations();
     app.SeedData();
     // app.MapOpenApi();
@@ -44,4 +49,29 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.MapHealthChecks("health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
 app.Run();
+
+public class CustomSqlHealthCheck(ISqlConnectionFactory sqlConnectionFactory) : IHealthCheck
+{
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            using var connection = sqlConnectionFactory.CreateConnection();
+
+            await connection.ExecuteScalarAsync("SELECT 1;");
+
+            return HealthCheckResult.Healthy();
+        }
+        catch (Exception e)
+        {
+            return HealthCheckResult.Unhealthy(exception: e);
+        }
+    }
+}
